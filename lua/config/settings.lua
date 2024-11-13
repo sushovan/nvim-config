d57588610276be99851b40547fd6a2067637f3e5
Sushovan Mukherjee <sushovan@defineway.com>
2024-11-14 04:49:11 +0530

    NVIM config primary commit
    Signed-off-by: Sushovan Mukherjee <sushovan@defineway.com>

---
+++ lua/config/settings.lua
@@ -0,0 +1,28 @@
+local global = vim.g
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.syntax = "on"
+o.autoindent = true
+o.smartindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+-- Highlight search as you type
+o.incsearch = true
+o.encoding = "UTF-8"
+o.ruler = true
+o.mouse = "a"
+o.title = true
+o.hidden = true
+o.ttimeoutlen = 0
+o.wildmenu = true
+o.showcmd = true
+o.showmatch = true
+o.inccommand = "split"
+o.splitright = true
+o.splitbelow = true
+o.termguicolors = true
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("nightfox").setup({
+			options = {
+				transparent = true,
+			},
+		})
+
+		--vim.cmd("colorscheme nightfox")
+		--vim.cmd("colorscheme dayfox")
+		--vim.cmd("colorscheme dawnfox")
+		--vim.cmd("colorscheme duskfox")
+		--vim.cmd("colorscheme nordfox")
+		--vim.cmd("colorscheme terafox")
+		vim.cmd("colorscheme carbonfox")
+	end,
+}
